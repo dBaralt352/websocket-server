4a9f2466938f7774329769d02237178cb05e9df2
dbaralt <dbaralt@portalteck.com>
2023-10-24 13:31:30 -0500

    Cambios para ws Helga

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const server = new ws.Server({
 
 server.on('connection', (ws, req) => {
   let origin=req.headers.origin;
+  console.log('connection', origin);
   CORS.validateClient(ws, req);
   ws.on('message', (message) => {
     Events.emitToOrigin(origin, message);
   });
   ws.on('close', () => {
+    console.log('close', origin);
     if(origin in Events.clients)
       Events.clients[origin].delete(ws);
   });
